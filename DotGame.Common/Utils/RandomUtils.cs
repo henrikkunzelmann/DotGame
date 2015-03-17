1c9979379552f9156a2f597af91e24c4fe4f16d2
Joex3 <git@joex3.net>
2015-03-17 08:59:29 +0100

    Farbkonstanten; Random.NextColor; Dokumentation;

--- DotGame.Common/Utils/RandomUtils.cs
+++ DotGame.Common/Utils/RandomUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotGame.Graphics;
 
 namespace DotGame.Utils
 {
@@ -33,6 +34,16 @@ namespace DotGame.Utils
         public static Vector3 NextVector3(this Random rand)
         {
             return new Vector3(rand.NextFloat(), rand.NextFloat(), rand.NextFloat());
+        }
+
+        /// <summary>
+        /// Gibt eine Farbe mit Zufallswerten zwischen 0.0f und 1.0f zurück.
+        /// </summary>
+        /// <param name="alpha1">Gibt an, ob der Alphaanteil mit 1.0f initialisiert werden soll.</param>
+        /// <returns>Die Farbe.</returns>
+        public static Color NextColor(this Random rand, bool alpha1 = true)
+        {
+            return new Color(rand.NextFloat(), rand.NextFloat(), rand.NextFloat(), alpha1 ? 1.0f : rand.NextFloat());
         }
     }
 }
