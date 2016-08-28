8e0a108621e321945f46e8d058dc4a8035b251e5
Henrik Kunzelmann <hkunzelmann@web.de>
2016-08-28 02:48:17 +0200

    Update packages

--- DotGame.DirectX11/Windows/GameControl.cs
+++ DotGame.DirectX11/Windows/GameControl.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DotGame.Graphics;
-using SharpDX.Windows;
-using SharpDX;
 using SharpDX.Direct3D;
-using SharpDX.Direct3D11;
 using Device = SharpDX.Direct3D11.Device;
 using SharpDX.DXGI;
 
